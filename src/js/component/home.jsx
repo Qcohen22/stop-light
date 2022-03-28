b0a6340de8b6fa853791296c8942366ad7a607e9
Qcohen22 <cohenjr.quincy@live.jobcorps.org>
2022-03-28 19:27:52 +0000

    light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [light, setLight] = useState("off");
+	let red = "bg-danger";
+	if (lightS === "off") setLight(red);
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div style={{ backgroundColor: "grey" }}>red</div>
+			<br></br>
+
+			<div style={{ backgroundColor: "grey" }}>green</div>
+			<br></br>
+			<div style={{ backgroundColor: "grey" }}>yellow</div>
+		</>
 	);
 };
 
